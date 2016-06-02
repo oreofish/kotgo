3f8a222e7039bd39a743d93cd3cbfb609b003fb1
Rick Xing <oreofish@gmail.com>
2016-06-02 13:51:41 +0800

    copy a new tunet

---
+++ tunet/src/main/java/com/meibug/tunet/Meizi.kt
@@ -0,0 +1,16 @@
+package cn.nekocode.kotgo.sample.data.dto
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+/**
+ * Created by nekocode on 16/3/3.
+ */
+data class Meizi(
+        val type: String,
+        val url: String,
+        val who: String
+) {
+
+}
